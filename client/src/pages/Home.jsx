2696328ba3a495f9e0ad5cf4a95d7e409157339e
ichsan1710 <ichsanmochammad@gmail.com>
2024-03-20 14:26:16 +0700

    for development purposes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import Container from "../components/container/container";
+import { useEffect, useState } from "react";
+import socket from "../socket.js";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.disconnect();
+    socket.auth = {
+      username: localStorage.username,
+    };
+
+    socket.connect();
+  });
+
   return (
     <>
       <div>
-        <Container/>
+        <Container />
       </div>
     </>
   );
