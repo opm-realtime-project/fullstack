892cd2082b84a0ceb426e96e1b08b1f63b5ee041
ichsan1710 <ichsanmochammad@gmail.com>
2024-03-20 20:51:11 +0700

    feat/chatApp-part5 done

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,71 +1,70 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom"
-import swal from 'sweetalert2'
-import axios from 'axios'
+import { Link, useNavigate } from "react-router-dom";
+import swal from "sweetalert2";
+import axios from "axios";
 
-function Form(){
-    const [input, setInput] = useState({
-        email: '',
-        password: ''
+function Form() {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChangeInput = (item) => {
+    const { name, value } = item.target;
+    setInput({
+      ...input,
+      [name]: value,
     });
+  };
 
-    const navigate = useNavigate();
+  const handleFormLogin = async (item) => {
+    item.preventDefault();
+    try {
+      const { data } = await axios({
+        method: "post",
+        url: "http://localhost:3000/login",
+        data: input,
+      });
 
-    const handleChangeInput = (item) => {
-        const {name, value} = item.target
-        setInput({
-            ...input,
-            [name]: value
-        })
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("user", data.user.username);
+
+      navigate("/");
+
+      swal.fire({
+        title: "Success login",
+        icon: "success",
+        timer: 1000,
+        showConfirmButton: false,
+      });
+    } catch (error) {
+      console.log(error);
+      swal.fire({
+        text: error.response.data.message,
+        icon: "error",
+        timer: 1000,
+        showConfirmButton: false,
+      });
     }
+  };
 
-    const handleFormLogin = async (item) => {
-        item.preventDefault();
-        try {
-            const { data } = await axios({
-                method: 'post',
-                url: 'http://localhost:3000/login',
-                data: input
-            })
-
-            localStorage.setItem('accessToken', data.accessToken)
-            localStorage.setItem('user', data.user.username)
-            
-            navigate("/");
-
-            swal.fire({
-                title: 'Success login',
-                icon: 'success',
-                timer: 1000,
-                showConfirmButton: false
-            })
-        } catch (error) {
-            console.log(error);
-            swal.fire({
-                text: error.response.data.message,
-                icon: "error",
-                timer: 1000,
-                showConfirmButton: false
-            })
-        }
-    }
-
-    return(
-        <>
-           <>
-  {/* component */}
-  <meta charSet="UTF-8" />
-  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link
-    rel="stylesheet"
-    href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
-    crossOrigin="anonymous"
-  />
-  <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-    <div
-      className="
+  return (
+    <>
+      {/* component */}
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+        crossOrigin="anonymous"
+      />
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <div
+          className="
     flex flex-col
     bg-white
     shadow-md
@@ -77,26 +76,24 @@ function Form(){
     rounded-3xl
     w-50
     max-w-md
-  "
-    >
-      <div className="font-medium self-center text-xl sm:text-3xl text-gray-800">
-        Welcome Back
-      </div>
-      <div className="mt-4 self-center text-xl sm:text-sm text-gray-800">
-        Enter your credentials to access your account
-      </div>
-      <div className="mt-10">
-        <form onSubmit={handleFormLogin}>
-          <div className="flex flex-col mb-5">
-            <label
-              htmlFor="email"
-              className="mb-1 text-xs tracking-wide text-gray-600"
-            >
-              E-Mail Address:
-            </label>
-            <div className="relative">
-              <div
-                className="
+  ">
+          <div className="font-medium self-center text-xl sm:text-3xl text-gray-800">
+            Welcome Back
+          </div>
+          <div className="mt-4 self-center text-xl sm:text-sm text-gray-800">
+            Enter your credentials to access your account
+          </div>
+          <div className="mt-10">
+            <form onSubmit={handleFormLogin}>
+              <div className="flex flex-col mb-5">
+                <label
+                  htmlFor="email"
+                  className="mb-1 text-xs tracking-wide text-gray-600">
+                  E-Mail Address:
+                </label>
+                <div className="relative">
+                  <div
+                    className="
               inline-flex
               items-center
               justify-center
@@ -106,15 +103,14 @@ function Form(){
               h-full
               w-10
               text-gray-400
-            "
-              >
-                <i className="fas fa-at text-blue-500" />
-              </div>
-              <input
-                id="email"
-                type="email"
-                name="email"
-                className="
+            ">
+                    <i className="fas fa-at text-blue-500" />
+                  </div>
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    className="
               text-sm
               placeholder-gray-500
               pl-10
@@ -125,21 +121,20 @@ function Form(){
               py-2
               focus:outline-none focus:border-blue-400
             "
-                placeholder="Enter your email"
-                onChange={handleChangeInput}
-              />
-            </div>
-          </div>
-          <div className="flex flex-col mb-6">
-            <label
-              htmlFor="password"
-              className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
-            >
-              Password:
-            </label>
-            <div className="relative">
-              <div
-                className="
+                    placeholder="Enter your email"
+                    onChange={handleChangeInput}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col mb-6">
+                <label
+                  htmlFor="password"
+                  className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">
+                  Password:
+                </label>
+                <div className="relative">
+                  <div
+                    className="
               inline-flex
               items-center
               justify-center
@@ -149,17 +144,16 @@ function Form(){
               h-full
               w-10
               text-gray-400
-            "
-              >
-                <span>
-                  <i className="fas fa-lock text-blue-500" />
-                </span>
-              </div>
-              <input
-                id="password"
-                type="password"
-                name="password"
-                className="
+            ">
+                    <span>
+                      <i className="fas fa-lock text-blue-500" />
+                    </span>
+                  </div>
+                  <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    className="
               text-sm
               placeholder-gray-500
               pl-10
@@ -170,15 +164,15 @@ function Form(){
               py-2
               focus:outline-none focus:border-blue-400
             "
-                placeholder="Enter your password"
-                onChange={handleChangeInput}
-              />
-            </div>
-          </div>
-          <div className="flex w-full">
-            <button
-              type="submit"
-              className="
+                    placeholder="Enter your password"
+                    onChange={handleChangeInput}
+                  />
+                </div>
+              </div>
+              <div className="flex w-full">
+                <button
+                  type="submit"
+                  className="
             flex
             mt-2
             items-center
@@ -194,51 +188,48 @@ function Form(){
             transition
             duration-150
             ease-in
-          "
-            >
-              <span className="mr-2 uppercase">Sign In</span>
-              <span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </span>
-            </button>
+          ">
+                  <span className="mr-2 uppercase">Sign In</span>
+                  <span>
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      viewBox="0 0 24 24"
+                      stroke="currentColor">
+                      <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                  </span>
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </div>
-    <div className="flex justify-center items-center mt-6">
-      <a
-        href="#"
-        target="_blank"
-        className="
+        </div>
+        <div className="flex justify-center items-center mt-6">
+          <a
+            href="#"
+            target="_blank"
+            className="
       inline-flex
       items-center
       text-gray-700
       font-medium
       text-xs text-center
-    "
-      >
-        <span className="ml-2">You don't have an account?</span>
-      </a>
-      <Link to={'/register'} href="#" className="text-xs ml-2 text-blue-500 font-semibold">
-        Register now
-      </Link>
-    </div>
-  </div>
-</>
-
-
-        </>
-    )
+    ">
+            <span className="ml-2">You don't have an account?</span>
+          </a>
+          <Link
+            to={"/register"}
+            href="#"
+            className="text-xs ml-2 text-blue-500 font-semibold">
+            Register now
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Form
+export default Form;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,28 +6,16 @@ function Navbar() {
     <>
       <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
         <div className="mb-2 sm:mb-0">
-          <a
-            href="#"
-            className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
+          <span className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
             Home
-          </a>
+          </span>
         </div>
         <div>
-          <a
-            href="#"
-            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">
-            One
-          </a>
-          <a
-            href="#"
-            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">
-            Two
-          </a>
           <button
             onClick={() => {
-              localStorage.removeItem("accessToken")
-              localStorage.removeItem("user")
-              navigate('/login')
+              localStorage.removeItem("accessToken");
+              localStorage.removeItem("user");
+              navigate("/login");
             }}
             className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">
             Logout
