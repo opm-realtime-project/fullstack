8ae26d68497940005ba880bc154086391803027a
ichsan1710 <ichsanmochammad@gmail.com>
2024-03-21 01:52:27 +0700

    feat/room-chatApp not done yet

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
       <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
         <div className="mb-2 sm:mb-0">
           <span className="text-2xl no-underline text-grey-darkest hover:text-blue-dark">
-            Home
+            Hello, {localStorage.user}
           </span>
         </div>
         <div>
